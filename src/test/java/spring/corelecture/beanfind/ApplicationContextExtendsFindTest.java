8e460c91868158395f1ebc4ea708dc533be7c24a
RedForest <74250270+RedForest0607@users.noreply.github.com>
2023-01-31 00:21:08 +0900

    :memo: setting spring with .xml

--- src/test/java/spring/corelecture/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/spring/corelecture/beanfind/ApplicationContextExtendsFindTest.java
@@ -1,6 +1,5 @@
 package spring.corelecture.beanfind;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
@@ -17,8 +16,8 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class ApplicaitonContextExtendsFindTest {
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+public class ApplicationContextExtendsFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
 
     @Test
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다.")
